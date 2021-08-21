33b0cbc75f4ead9afda5c038b924df6fb3bbb532
Yassine Boutahar <yassineboutahar@cmail.carleton.ca>
2021-08-21 17:52:53 -0400

    Added new role, updated logos

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -2,13 +2,15 @@ import React from "react";
 import WideContainer from "./WideContainer";
 import AboutBlock from "../components/AboutBlock";
 import { makeStyles, useMediaQuery, Typography } from "@material-ui/core";
-import amazonLogo from "../images/amazon.png";
-import carletonLogo from "../images/smaller_carleton.png";
-import gocLogo from "../images/smaller_goc.png";
+import shopifyLogo from "../images/shopify.png";
+import alexaLogo from "../images/alexa.png";
+import carletonLogo from "../images/carleton.png";
+import gocLogo from "../images/goc.png";
 import signiantLogo from "../images/smaller_signiant.png";
 import apptionLogo from "../images/smaller_apption.png";
 import bd2iLogo from "../images/smaller_bd2i.png";
-import mobileAmazonLogo from "../images/mobile_amazon.png";
+import mobileShopifyLogo from "../images/mobile_shopify.png";
+import mobileAlexaLogo from "../images/mobile_alexa.png";
 import mobileCarletonLogo from "../images/mobile_carleton.png";
 import mobileGocLogo from "../images/mobile_goc.png";
 import mobileSigniantLogo from "../images/mobile_signiant.png";
@@ -32,7 +34,8 @@ const useStyles = makeStyles((theme) => ({
 const AboutMe = (props) => {
   const classes = useStyles();
   const desktop = useMediaQuery("(min-width:1020px)");
-  let amazon = desktop ? amazonLogo : mobileAmazonLogo;
+  let shopify = desktop ? shopifyLogo : mobileShopifyLogo;
+  let alexa = desktop ? alexaLogo : mobileAlexaLogo;
   let carleton = desktop ? carletonLogo : mobileCarletonLogo;
   let signiant = desktop ? signiantLogo : mobileSigniantLogo;
   let apption = desktop ? apptionLogo : mobileApptionLogo;
@@ -41,7 +44,7 @@ const AboutMe = (props) => {
 
   return (
     <div id="AboutMe">
-      <WideContainer>
+      <WideContainer variant="grey">
         <AboutBlock
           img={carleton}
           alt="Carleton University"
@@ -49,30 +52,60 @@ const AboutMe = (props) => {
           link="https://carleton.ca/scs/"
         >
           <Typography variant="subtitle1" className={classes.description}>
-            I am a third year student in Carleton University's Computer Science
-            Honours program with a minor in psychology. I am currently working
-            from home in Toronto and I am seeking internship opportunities for
-            the 2021 Fall co-op term.
+            I am a fourth year student in Carleton University's Computer Science
+            Honours program with a minor in psychology. I have picked up many frontend
+            and backend skills through school and my internship experiences and am
+            passionate about applying them to my own personal projects.
+          </Typography>
+        </AboutBlock>
+      </WideContainer>
+
+      <WideContainer>
+        <AboutBlock
+          img={shopify}
+          alt="Shopify"
+          title="Fall 2021"
+          link="https://www.shopify.ca/"
+        >
+          <Typography variant="subtitle1" className={classes.description}>
+            Incoming Backend Developer Intern at{" "}
+            <a
+              className={classes.a}
+              href="https://www.shopify.ca/"
+            >
+              <b>Shopify</b>
+            </a>{" "}
+            on the Product Discovery team in the Core Product org.
           </Typography>
         </AboutBlock>
       </WideContainer>
 
       <WideContainer variant="grey">
         <AboutBlock
-          img={amazon}
-          alt="Amazon"
+          img={alexa}
+          alt="Alexa"
           title="Summer 2021"
           link="https://www.amazon.com/alexa-drop-in-calling-intercom/b?ie=UTF8&node=21393410011"
         >
           <Typography variant="subtitle1" className={classes.description}>
-            Incoming Software Developer Engineer intern at{" "}
+            Worked as a Software Developer Engineer intern at{" "}
             <a
               className={classes.a}
               href="https://www.amazon.com/alexa-drop-in-calling-intercom/b?ie=UTF8&node=21393410011"
             >
               <b>Amazon</b>
             </a>{" "}
-            under the Devices organization.
+            under the Alexa Communications org. Designed and implemented my own intern project
+            tracking EC2 usage of my team's internal customers through a serverless backend,
+            displaying usage statistics through a frontend console and offloading any related AWS bills.
+            <ul className={classes.ul}>
+              <li>Frontend development - React, AWS Polaris</li>
+              <li>
+                Backend development - Java, Python, Typescript, Node.js, GraphQL
+              </li>
+              <li>AWS - EC2, Lambda, Timestream, CDK, EventBridge, CloudWatch</li>
+              <li>Testing - Jest, Mockito, Internal Integ Test Tool</li>
+            </ul>
           </Typography>
         </AboutBlock>
       </WideContainer>
